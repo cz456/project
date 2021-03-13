a09d845c7dbfebbe7a470903e67495ea667a9d96
chenzhen <chenzhen@cnoocraiborn.com>
2021-03-13 17:00:34 +0800

    3.3

--- book/book.sql
+++ book/book.sql
@@ -1,39 +1,55 @@
-/*
- Navicat Premium Data Transfer
+create table book
+(
+    id           int auto_increment
+        primary key,
+    bookName     varchar(128) null,
+    bookNumber   int          null,
+    rent         double       null,
+    kinds        varchar(128) null,
+    introduction varchar(128) null,
+    author       varchar(128) null,
+    url          varchar(128) null,
+    urls         varchar(128) null
+);
 
- Source Server         : a
- Source Server Type    : MySQL
- Source Server Version : 50722
- Source Host           : localhost:3306
- Source Schema         : book
+create table bookcategory
+(
+    id       int auto_increment
+        primary key,
+    bookType varchar(128) null,
+    bookCode varchar(128) null
+);
 
- Target Server Type    : MySQL
- Target Server Version : 50722
- File Encoding         : 65001
+create table bookdetails
+(
+    id       int auto_increment
+        primary key,
+    bookId   int        null,
+    personId int        null,
+    bookDate timestamp  null,
+    state    varchar(1) null
+);
 
- Date: 23/01/2021 14:26:36
-*/
+create table person
+(
+    id          int auto_increment
+        primary key,
+    name        varchar(128) null,
+    loginNumber varchar(128) null,
+    passWord    varchar(128) null,
+    role        varchar(128) null,
+    state       varchar(1)   null,
+    note        varchar(128) null
+);
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+create table user
+(
+    id          int auto_increment
+        primary key,
+    userName    varchar(128) null,
+    loginNumber varchar(128) null,
+    phoneNumber varchar(128) null,
+    mailbox     varchar(128) null,
+    note        varchar(128) null
+);
 
--- ----------------------------
--- Table structure for person
--- ----------------------------
-DROP TABLE IF EXISTS `person`;
-CREATE TABLE `person`  (
-  `id` int(11) DEFAULT NULL,
-  `name` varchar(1024) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT '姓名',
-  `loginNumber` varchar(1024) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT '登录学号/工号',
-  `passWord` varchar(1024) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT '密码',
-  `role` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT '角色',
-  `state` varchar(1) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT '状态',
-  `note` varchar(1024) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT '备注'
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '人员表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of person
--- ----------------------------
-INSERT INTO `person` VALUES (1, 'cz', '001', '001', '1', '1', NULL);
-
-SET FOREIGN_KEY_CHECKS = 1;
